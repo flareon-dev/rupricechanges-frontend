00c029600cc6f81f51ac14147b2cf68d605669e9
alex <megamanok99@yandex.ru>
2022-03-19 20:11:18 +0300

    update 2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import './App.less';
 import SiderPart from './components/Sider';
 import test from './test.svg';
 import moment from 'moment';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
 const { Header, Footer, Sider, Content } = Layout;
 const { Option } = Select;
 const { Text, Link } = Typography;
@@ -85,41 +96,38 @@ function App() {
   }
   console.log(currentShop);
   ////
-  const ctx = document.getElementById('myChart').getContext('2d');
-  // const myChart = new Chart(ctx, {
-  //     type: 'bar',
-  //     data: {
-  //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  //         datasets: [{
-  //             label: '# of Votes',
-  //             data: [12, 19, 3, 5, 2, 3],
-  //             backgroundColor: [
-  //                 'rgba(255, 99, 132, 0.2)',
-  //                 'rgba(54, 162, 235, 0.2)',
-  //                 'rgba(255, 206, 86, 0.2)',
-  //                 'rgba(75, 192, 192, 0.2)',
-  //                 'rgba(153, 102, 255, 0.2)',
-  //                 'rgba(255, 159, 64, 0.2)'
-  //             ],
-  //             borderColor: [
-  //                 'rgba(255, 99, 132, 1)',
-  //                 'rgba(54, 162, 235, 1)',
-  //                 'rgba(255, 206, 86, 1)',
-  //                 'rgba(75, 192, 192, 1)',
-  //                 'rgba(153, 102, 255, 1)',
-  //                 'rgba(255, 159, 64, 1)'
-  //             ],
-  //             borderWidth: 1
-  //         }]
-  //     },
-  //     options: {
-  //         scales: {
-  //             y: {
-  //                 beginAtZero: true
-  //             }
-  //         }
-  //     }
-  // });
+  ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => 30),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => 40),
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
   ////
   return (
     <Layout style={{ height: '100%' }}>
@@ -143,9 +151,7 @@ function App() {
           currentShop={currentShop}
         />
         <Content>
-          <div id="chartDiv1" style={{ width: '100%', height: '500px' }}>
-            {' '}
-          </div>
+          <Bar options={options} data={data} />
 
           <Table dataSource={infoItem} columns={columns} />
         </Content>
